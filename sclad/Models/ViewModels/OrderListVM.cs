66476807acc09fe2f038130bb7d7bc21b3a4e6d5
ikvankan <ikvankan@gmail.com>
2023-11-09 22:46:48 +0300

    OrderController and OrderlistVM ADD

---
+++ sclad/Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace sclad.Models.ViewModels
+{
+    public class OrderListVM
+    {
+        public IEnumerable<OrderHeader> OrderHList { get; set; }
+        public IEnumerable<SelectListItem> SelectList { get; set; }
+        public string Status { get; set; }
+    }
+}
