35708a65e8eaf13a957a21c9dd284c0b8a2d24bc
ikvankan <ikvankan@gmail.com>
2023-11-11 00:08:44 +0300

    validation fix

--- sclad/Controllers/OrderController.cs
+++ sclad/Controllers/OrderController.cs
@@ -15,7 +15,7 @@ namespace sclad.Controllers
             _db = db;
             _brain = brain;
         }
-        public IActionResult Index()
+        public IActionResult Index(string searchName = null, string searchEmail = null, string searchPhone = null, string Status = null)
         {
             OrderListVM orderListVM = new OrderListVM()
             {
@@ -26,6 +26,22 @@ namespace sclad.Controllers
                     Value = i
                 })
             };
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.FullName.ToLower().Contains(searchName.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchEmail))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u=>u.Email.ToLower().Contains(searchEmail.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchPhone))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.PhoneNumber.ToLower().Contains(searchPhone.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(Status)&& Status!= "--Order Status--")
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.OrderStatus.ToLower().Contains(Status.ToLower()));
+            }
             return View(orderListVM);
         }
     }
